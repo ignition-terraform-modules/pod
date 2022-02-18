ab1e46426ee48c8c8091722bf4ad32756aa6c69d
Nick Curry <code@nickcurry.com>
2022-02-18 13:10:40 -0600

    feat: Add additional outputs for systemd unit name and pod name

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,13 @@ output "ignition" {
   value = data.template_file.ignition.rendered
   sensitive = true
 }
+
+output "systemd_service_name" {
+  description = "The name of the pod create systemd service file."
+  value = "${var.name}-pod-create.service"
+}
+
+output "pod_name" {
+  description = "The name of the pod."
+  value = var.name
+}
